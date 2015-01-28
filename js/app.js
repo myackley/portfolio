6511c8a98b4f3f3df37d3c89d55b3395e66c868d
Mark Yackley <myackley@hotmail.com>
2015-01-27 22:52:14 -0600

    Added code to auto-close the drop down menu when a link is clicked and reassigned id 'hooks' for the navigation to compensate for the padding at the top of each section

--- js/app.js
+++ js/app.js
@@ -6,4 +6,9 @@ $(document).ready(function() {
 		$(this).siblings('.nav-menu').toggleClass('hide');
 	});
 
+	$('.nav .container').on('click','.active + ul li a', function() {
+		$('.mobmenu').toggleClass('active');
+		$('.nav-menu').toggleClass('hide');
+	});
+
 });
